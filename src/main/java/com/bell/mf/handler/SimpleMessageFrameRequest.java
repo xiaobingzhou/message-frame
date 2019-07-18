8bef3d15d6eef08e10b9a31a2bf6f080e7d430c0
bing <279414377@qq.com>
2019-07-18 19:53:31 +0800

    添加lombok包，是@Builder注解

--- src/main/java/com/bell/mf/handler/SimpleMessageFrameRequest.java
+++ src/main/java/com/bell/mf/handler/SimpleMessageFrameRequest.java
@@ -5,6 +5,11 @@ import java.util.Date;
 
 import com.bell.mf.IMessageFrame;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class SimpleMessageFrameRequest implements Serializable, MessageFrameRequest{
 	/**
 	 * 
@@ -12,33 +17,7 @@ public class SimpleMessageFrameRequest implements Serializable, MessageFrameRequ
 	private static final long serialVersionUID = 3358325538683680300L;
 	
 	private String deviceId;
-	private IMessageFrame iMessageFrame;
+	private IMessageFrame messageFrame;
 	private String message;
 	private Date systemDate;
-	
-	public String getDeviceId() {
-		return deviceId;
-	}
-	public void setDeviceId(String deviceId) {
-		this.deviceId = deviceId;
-	}
-	public IMessageFrame getMessageFrame() {
-		return iMessageFrame;
-	}
-	public void setMessageFrame(IMessageFrame iMessageFrame) {
-		this.iMessageFrame = iMessageFrame;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public Date getSystemDate() {
-		return systemDate;
-	}
-	public void setSystemDate(Date systemDate) {
-		this.systemDate = systemDate;
-	}
-	
 }
