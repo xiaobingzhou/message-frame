536ac2766b52e9badd5337f02443fba6968dae4a
bell.zhouxiaobing <bell.zhouxiaobing@staginfo.com>
2020-04-28 08:59:32 +0800

    添加MessageFrameHandlerInterceptor接口适配器InterceptorAdaptor；配置启用注解，可以一键开启功能

---
+++ src/main/java/com/bell/mf/annotation/EnableMessageFrameHandler.java
@@ -0,0 +1,18 @@
+package com.bell.mf.annotation;
+
+import com.bell.mf.support.MessageFrameHandlerAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 启用MessageFrameHandler功能
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Import(MessageFrameHandlerAutoConfiguration.class)
+public @interface EnableMessageFrameHandler {
+}
