a512fca796e2cd731c7062104e91b6301143cefe
bell.zhouxiaobing <bell.zhouxiaobing@staginfo.com>
2020-04-29 22:35:48 +0800

    delete filter package

--- src/main/java/com/bell/mf/filter/Filter.java
+++
@@ -1,7 +0,0 @@
-package com.bell.mf.filter;
-
-import com.bell.mf.handler.MessageFrameRequest;
-
-public interface Filter {
-	 public void doFilter(MessageFrameRequest request, FilterChain filterChain);
-}
